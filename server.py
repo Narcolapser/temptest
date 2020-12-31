1085c01af71a588450a947a61cc9bfea01efd161
Toben Archer <sandslash@gmail.com>
2020-12-31 08:38:55 -0600

    Well lets see if this works.

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello World'
+
+@app.route('/', methods=['POST'])
+def hook():
+	print(request.data)
+	return '{"status":"success"}'
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=3000)
